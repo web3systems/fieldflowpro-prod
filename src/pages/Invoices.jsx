b5849c0ea0d2b58ebab120a8134a7b24474a3424
Tim Parrow <tparrow78@gmail.com>
2026-03-14 16:59:44 +0000

    File changes

--- src/pages/Invoices.jsx
+++ src/pages/Invoices.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { base44 } from "@/api/base44Client";
 import { useApp } from "../Layout";
 import {
