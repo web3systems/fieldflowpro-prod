886b169cd16b5578b45a92f934acbdc5c95aa84c
Tim Parrow <tparrow78@gmail.com>
2026-03-15 02:06:45 +0000

    File changes

--- src/pages/JobDetail.jsx
+++ src/pages/JobDetail.jsx
@@ -15,7 +15,6 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { format } from "date-fns";
 import CustomerNotesSection from "../components/jobs/CustomerNotesSection";
 import InternalNotesSection from "../components/jobs/InternalNotesSection";
