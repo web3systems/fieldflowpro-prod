11c39c3d39bcea67eacc9a9e1b51fc837b78abd9
Tim Parrow <tparrow78@gmail.com>
2026-03-25 17:23:09 +0000

    File changes

--- base44/functions/subscriptionWebhook/entry.ts
+++ base44/functions/subscriptionWebhook/entry.ts
@@ -1,4 +1,4 @@
-import { createClientFromRequest } from 'npm:@base44/sdk@0.8.20';
+import { createClientFromRequest } from 'npm:@base44/sdk@0.8.23';
 import Stripe from 'npm:stripe@14.21.0';
 
 const stripe = new Stripe(Deno.env.get('STRIPE_SECRET_KEY'));
